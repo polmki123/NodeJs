92f2e15cc297fcb8a651e23117c25a41da632f1c
kch1024 <ok6403@gmail.com>
2021-02-03 01:33:06 +0900

    2021

---
+++ 3강 Express/08.Express_NunjucksMacro/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const admin = require('./routes/admin');
+
+const app = express();
+const port = 3000;
+
+nunjucks.configure('template', {
+    autoescape: true,
+    express: app,
+});
+
+// 미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//업로드 path 추가
+app.use('/uploads', express.static('uploads'));
+
+// 템플릿 변수
+// app.use((req, res, next) => {
+//     app.locals.isLogin = true;
+//     next();
+// });
+
+// 템플릿 변수
+app.use((req, res, next) => {
+    app.locals.isLogin = false;
+    app.locals.req_path = req.path; //현재 req 의 path를 보내주는 기능
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('express start');
+});
+
+// Routing
+app.use('/admin', admin);
+
+// 404 페이지가 없습니다.
+app.use((req, res, _) => {
+    // _ 를 쓰는 이유는 사용하지 않아서 이다.
+    res.status(404).render('common/404.html');
+});
+
+// 500
+app.use((err, req, res, _) => {
+    res.status(500).render('common/500.html');
+});
+
+app.listen(port, () => {
+    console.log('Express listening on port', port);
+});
